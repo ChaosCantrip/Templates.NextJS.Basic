2c79a0761e352a2e658ca4e6821ea89bc3fb928a
ChaosCantrip <dev@chaoscantrip.com>
2024-12-25 01:24:46 +0000

    Added sitemap.js

---
+++ src/app/sitemap.js
@@ -0,0 +1,10 @@
+export default function sitemap() {
+    return [
+        {
+            url: "https://CHANGEME/",
+            lastModified: new Date(),
+            changeFrequency: "yearly",
+            priority: 1.0
+        }
+    ]
+}
