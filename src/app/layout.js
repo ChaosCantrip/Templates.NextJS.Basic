374ad7a66161f9f7106f2e716e05f5041bff834e
ChaosCantrip <dev@chaoscantrip.com>
2024-12-25 01:27:05 +0000

    Added metadata

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,6 +2,10 @@ import "@/styles/global/master.css";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 
+export const metadata = {
+    title: "New Website"
+}
+
 export default function RootLayout({ children }) {
     return (
         <html>
