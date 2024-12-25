4e50e6dfa738cc252915fef742eacbc732223f7e
ChaosCantrip <dev@chaoscantrip.com>
2024-12-25 01:07:51 +0000

    Added basic RootLayout and HomePage

---
+++ src/app/layout.js
@@ -0,0 +1,11 @@
+export default function RootLayout({ children }) {
+    return (
+        <html>
+            <body>
+                <main>
+                    {children}
+                </main>
+            </body>
+        </html>
+    );
+}
