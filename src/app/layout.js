7aecc2704039334de44992390510f135d44565ac
ChaosCantrip <dev@chaoscantrip.com>
2024-12-25 01:18:01 +0000

    Added Header and Footer

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,12 +1,16 @@
 import "@/styles/global/master.css";
+import Header from "@/components/Header";
+import Footer from "@/components/Footer";
 
 export default function RootLayout({ children }) {
     return (
         <html>
             <body>
+                <Header />
                 <main>
                     {children}
                 </main>
+                <Footer />
             </body>
         </html>
     );
